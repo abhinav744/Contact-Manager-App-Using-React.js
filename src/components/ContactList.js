d4a02259057924dad826208714e65271ce7379a1
HP <abhinavchauhan744@gmail.com>
2024-11-02 23:52:36 +0530

    first commit

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,13 +1,15 @@
 import React from "react";
+import ContactCard from "./ContactCard";
 
 const ContactList = (props) => {
     console.log(props);
+    const renderContactList = props.contacts.map((contact) => {
+        return <ContactCard contact={contact}></ContactCard>
+    });
     
     return (
-        <div className="ui celled list">
-            Contact List
-        </div>
+        <div className="ui celled list">{renderContactList}</div>
     );
 }
 
-export default ContactList
+export default ContactList;
